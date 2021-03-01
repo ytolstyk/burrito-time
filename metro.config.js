d925c2d6378fa60ec1b473d5826d5c192d369bca
yuriy-manifold <yuriy@manifoldtechnology.com>
2021-03-01 13:37:06 -0800

    modifying stuff

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,11 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
 module.exports = {
+  resolver: {
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'expo.js', 'expo'],
+  },
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
